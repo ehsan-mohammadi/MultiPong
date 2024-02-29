3793d8db68cc9035b660668c798ba47ff4f24796
Ehsan <mohammadi.ehsan1994@gmail.com>
2024-02-29 15:08:04 +0330

    Add basis of Events

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace MultiPong.Managers
+{
+    using Services;
+    using Events;
+
+    public class EventManager: IService
+    {
+        private readonly List<IEventListener> listeners;
+
+        public EventManager()
+        {
+            this.listeners = new List<IEventListener>();
+            ServiceLocator.Register(this);
+        }
+
+        public void Register(IEventListener listener)
+        {
+            if (listeners.Contains(listener))
+                return;
+            
+            listeners.Add(listener);
+        }
+
+        public void Unregister(IEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+
+        public void Propagate(IEvent evt, object sender)
+        {
+            foreach(var listener in listeners)
+                listener.OnEvent(evt, sender);
+        }
+    }
+}
